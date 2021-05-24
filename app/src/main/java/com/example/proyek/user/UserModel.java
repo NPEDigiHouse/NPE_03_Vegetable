827c256255fc581321c6183721cc98c6784c5e4d
Richard Enrico <richardenrico02@gmail.com>
2021-05-24 18:49:04 +0800

    change User to UserModel and move to user package

---
+++ app/src/main/java/com/example/proyek/user/UserModel.java
@@ -0,0 +1,47 @@
+package com.example.proyek.user;
+
+public class UserModel {
+    public  String username, email, phoneNumber, as;
+
+    public UserModel(String username, String email, String phoneNumber, String as){
+        this.username = username;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.as = as;
+    }
+
+    public UserModel() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAs() {
+        return as;
+    }
+
+    public void setAs(String as) {
+        this.as = as;
+    }
+}
